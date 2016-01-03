713c4d8c99febd73f6c858cb9205ba9415f69ab4
Manuel Graf <mangra@users.noreply.github.com>
2016-01-03 20:46:02 +0100

    getting page titles for listed rdbms from rdbms list wikipedia page

--- src/dbms_stats_clj/core.clj
+++ src/dbms_stats_clj/core.clj
@@ -28,14 +28,29 @@
 
 (defn get-infobox-content
   "gets infobox content from wikipedia page"
-  [url]
+  [pageid]
   (clean-infolines (drop-last (subvec (str/split (re-find (re-pattern "(?s)\\{\\{Infobox.*?\\}\\}\n\n")
-                                                          (get-in (json/read-str (:body (http-client/get url)))
+                                                          (get-in (json/read-str (:body (http-client/get (str "https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=wikitext&format=json&redirects=true&pageid=" pageid))))
                                                                   ["parse" "wikitext" "*"]))
                                                  #"\n")
                                       1))))
 
+(defn get-rdbms-systems-pagetitles
+  "asdf"
+  []
+  (map #(get % 0) (map #(str/split % #"\|") (map #(str/replace % #"[\*\[\]\n]" "")
+                                          (re-seq #"\*.*?\n"
+                                                  (get-in (json/read-str (:body (http-client/get "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=List_of_relational_database_management_systems&prop=revisions&rvprop=content&rvsection=1")))
+                                                          ["query" "pages" "1568820" "revisions" 0 "*"]))))))
+(defn get-page-id
+  "gets the corresponding pageid to a wikipedia title"
+  [pagetitle]
+  (keys (get-in (json/read-str (:body (http-client/get (str "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=" pagetitle)))) ["query" "pages"])))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (get-infobox-content "https://en.wikipedia.org/w/api.php?action=parse&pageid=244884&section=0&prop=wikitext|parsetree&format=json"))
+  ;(get-infobox-content "244884")
+  ;(get-page-id "4th Dimension (Software)")
+  (def pageids (pmap #(get-page-id %) (get-rdbms-systems-pagetitles)))
+  (map #(println (type %)) pageids))
